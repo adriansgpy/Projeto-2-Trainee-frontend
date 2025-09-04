0976955ca1a54ea6cc85041d24e1c3b797e38f1f
Adrian <95186647+adriansgpy@users.noreply.github.com>
2025-09-04 09:42:08 -0300

    Feat(Intro): Tela de introdução adicionada
    Tela e componentes de introdução do RPG foram adicionadas.

--- LastCommit/src/frontend/src/app/app-routing.module.ts
+++ LastCommit/src/frontend/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { SignupComponent } from './components/auth/signup/signup.component';
+import { IntroComponent } from './components/intro/intro.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', component: IntroComponent },  
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent }
 ];
