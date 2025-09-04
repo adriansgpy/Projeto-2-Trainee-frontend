57db933db4d153c04878214003bb6997c785d7d2
Adrian <95186647+adriansgpy@users.noreply.github.com>
2025-09-04 13:14:53 -0300

    Feat(Login): Animação de mensagens
    Animação de mensagens obscuras na tela de login

--- LastCommit/src/frontend/src/app/components/auth/login/login.component.ts
+++ LastCommit/src/frontend/src/app/components/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
 import { CommonModule } from '@angular/common';
@@ -9,11 +9,70 @@ import { HttpClientModule } from '@angular/common/http';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   standalone: true,
-  imports: [CommonModule, HttpClientModule] // <- necessário
+  imports: [CommonModule, HttpClientModule] 
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit{
   username: string = '';
   password: string = '';
 
   constructor(private authService: AuthService, private router: Router) {}
+
+  ngAfterViewInit() {
+    this.animateGhostMessages();
+    this.startGlitch();
+  }
+
+  //frases aleatórias na tela
+  animateGhostMessages() {
+  const messages = document.querySelectorAll('.ghost-msg');
+
+  const animate = () => {
+    messages.forEach(msg => {
+      const msgEl = msg as HTMLElement;
+      const maxX = window.innerWidth - msgEl.offsetWidth;
+      const maxY = window.innerHeight - msgEl.offsetHeight;
+
+      const x = Math.random() * maxX;
+      const y = Math.random() * maxY;
+
+      msgEl.style.left = `${x}px`;
+      msgEl.style.top = `${y}px`;
+    });
+
+    setTimeout(() => requestAnimationFrame(animate), 2000 + Math.random() * 1000);
+  };
+
+  requestAnimationFrame(animate);
+}
+
+
+
+
+  // Glitch da tela
+  startGlitch() {
+    const glitch = document.querySelector('.tv-glitch') as HTMLElement;
+
+    const loop = () => {
+      const delay = 5000 + Math.random() * 5000; 
+      setTimeout(() => {
+        glitch.style.opacity = '1';
+        glitch.style.animation = 'glitch-flicker 0.5s linear';
+        setTimeout(() => {
+          glitch.style.opacity = '0';
+          glitch.style.animation = '';
+        }, 500 + Math.random() * 500); 
+        loop();
+      }, delay);
+    };
+
+    loop();
+  }
+
+
+  createAccount() {
+ 
+    this.router.navigate(['/signup']); 
+  // abrir tela de cadastro
+  }
+
 }
